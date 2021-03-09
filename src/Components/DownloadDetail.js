f924a1be8136806212f0b79ce23941aec86fadfe
Zane Frantzen <frantzen.zane@gmail.com>
2021-03-08 16:03:27 -0800

    feat: added input entered checks to download / upload popovers

--- src/Components/DownloadDetail.js
+++ src/Components/DownloadDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Text,
     View
@@ -12,15 +12,35 @@ const DownloadDetail = () => {
 
     var downloadInfo = useSelector((state) => state.dashboard).downloadInfo;
 
+    const [passwordEntered, setPasswordEntered] = useState(false);
+    const [accessCodeEntered, setAccessCodeEntered] = useState(false);
+    const [buttonsEnabled, setButtonsEnabled] = useState(false); 
+
+    const passwordCallback = () => {
+        setPasswordEntered(true);
+
+        if (accessCodeEntered) {
+            setButtonsEnabled(true);
+        }   
+    }
+    
+    const accessCodeCallback = () => {
+        setAccessCodeEntered(true);
+
+        if (passwordEntered) {
+            setButtonsEnabled(true);
+        }
+    }
+
     return (
         <View style={[Layout.column, Layout.alignItemsCenter, Gutters.largexlHPadding]} >
             <Text style={[Fonts.detailFileName, Gutters.regularxlVPadding]}>access {downloadInfo.fileName}</Text>
             <View style={[Layout.column, Layout.alignItemsCenter, Layout.justifyContentBetween, {height: 190}]}>
-                <InputField placeholder={"device password"} hideInput={true} fieldId={3} useLightInput={true} />
-                <InputField placeholder={"displayed device access code"} fieldId={4} useLightInput={true} />
+                <InputField placeholder={"device password"} hideInput={true} fieldId={3} useLightInput={true} finishEditingCallback={passwordCallback} />
+                <InputField placeholder={"displayed device access code"} fieldId={4} useLightInput={true} finishEditingCallback={accessCodeCallback} />
                 <View style={[Layout.row, Layout.alignItemsCenter]}>
-                    <Button title={"download"} color={Colors.secondary} style={[Gutters.regularRPadding, {flex: 2}]} />
-                    <Button title={"delete"} color={Colors.red} style={{flex: 1}} />
+                    <Button title={"download"} color={Colors.secondary} style={[Gutters.regularRPadding, {flex: 2}]} setEnabled={buttonsEnabled} />
+                    <Button title={"delete"} color={Colors.red} style={{flex: 1}} setEnabled={buttonsEnabled} />
                 </View>
             </View>
         </View>
--- src/Components/UploadDetail.js
+++ src/Components/UploadDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Text,
     View
@@ -9,18 +9,56 @@ import { useTheme } from '@/Theme'
 const UploadDetail = () => {
     const { Layout, Common, Fonts, Images, Gutters, Colors } = useTheme();
 
+    const [nameEntered, setNameEntered] = useState(false);
+    const [fileSelected, setFileSelected] = useState(false);
+    const [passwordEntered, setPasswordEntered] = useState(false);
+    const [accessCodeEntered, setAccessCodeEntered] = useState(false);
+    const [buttonEnabled, setButtonEnabled] = useState(false); 
+
+    const [fileSelectorTitle, setFileSelectorTitle] = useState("select file"); 
+
+    const fileNameCallback = () => {
+        setNameEntered(true);
+        if (fileSelected && passwordEntered && accessCodeEntered)
+            setButtonEnabled(true);
+    }
+
+    const fileSelectorCallback = (fileName) => {
+        // TODO: Remove else case once file selector is properly implemented
+        if (fileName !== undefined)
+            setFileSelectorTitle(fileName);
+        else
+            setFileSelectorTitle("testFile.pdf");
+
+        setFileSelected(true);
+        if (nameEntered && passwordEntered && accessCodeEntered)
+            setButtonEnabled(true);
+    }
+
+    const passwordCallback = () => {
+        setPasswordEntered(true);
+        if (nameEntered && fileSelected && accessCodeEntered)
+            setButtonEnabled(true);
+    }
+    
+    const accessCodeCallback = () => {
+        setAccessCodeEntered(true);
+        if (nameEntered && fileSelected && passwordEntered)
+            setButtonEnabled(true);
+    }
+
     return (
         <View style={[Layout.column, Layout.alignItemsCenter, Gutters.largexlHPadding]} >
             <Text style={[Fonts.detailFileName, Gutters.regularxlVPadding]}>upload new file</Text>
             <View style={[Layout.column, Layout.alignItemsCenter, Layout.justifyContentBetween, {height: 320}]}>
-                <InputField placeholder={"file name"} fieldId={3} useLightInput={true} />
+                <InputField placeholder={"file name"} fieldId={3} useLightInput={true} finishEditingCallback={fileNameCallback} />
                 <View style={[Layout.row, Layout.alignItemsCenter]}>
-                    <Button title={"select file"} useInputFieldStyle={true} style={Layout.fill} />
+                    <Button title={fileSelectorTitle} useInputFieldStyle={true} style={Layout.fill} clickCallback={fileSelectorCallback} />
                 </View>
-                <InputField placeholder={"device password"} hideInput={true} fieldId={5} useLightInput={true} />
-                <InputField placeholder={"displayed device access code"} fieldId={6} useLightInput={true} />
+                <InputField placeholder={"device password"} hideInput={true} fieldId={5} useLightInput={true} finishEditingCallback={passwordCallback} />
+                <InputField placeholder={"displayed device access code"} fieldId={6} useLightInput={true} finishEditingCallback={accessCodeCallback} />
                 <View style={[Layout.row, Layout.alignItemsCenter]}>
-                    <Button title={"upload file"} color={Colors.secondary} style={Layout.fill} />
+                    <Button title={"upload file"} color={Colors.secondary} style={Layout.fill} setEnabled={buttonEnabled} />
                 </View>
             </View>
         </View>
