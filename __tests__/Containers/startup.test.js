e045edd77c9f86f2b5d35a431a11c662f2d82d02
Zane Frantzen <frantzen.zane@gmail.com>
2021-03-29 11:27:17 -0700

    fix: modifed import for CI compatability

--- __tests__/Containers/startup.test.js
+++ __tests__/Containers/startup.test.js
@@ -1,12 +1,12 @@
 import 'react-native'
 import React from 'react'
-import Startup from '../../src/Containers/Startup'
+import { IndexStartupContainer } from '../../src/Containers'
 import { render } from '@testing-library/react-native'
 
 import { store } from '../../src/Store'
 import { Provider } from 'react-redux'
 
 it('renders and matches snapshot', () => {
-  const { toJSON } = render(<Provider store={store}><Startup /></Provider>);
+  const { toJSON } = render(<Provider store={store}><IndexStartupContainer /></Provider>);
   expect(toJSON()).toMatchSnapshot();
 })
