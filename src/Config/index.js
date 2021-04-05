63540df9a623e6daa8881e6ec31d6b8ee8bf7f8c
Zane Frantzen <frantzen.zane@gmail.com>
2021-04-04 18:06:19 -0700

    fix: updated some config values

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,7 +1,7 @@
 export const Config = {
   mocking: {
     apiConnection: false,
-    deviceConnection: true,
+    deviceConnection: false,
   },
 
   api: {
@@ -9,7 +9,7 @@ export const Config = {
   },
 
   device: {
-    expectedBluetoothName: "DE1_DEVICE", // Expected discoverable device name
+    expectedBluetoothName: "HC-05", // Expected discoverable device name
     connectionTimeout: 120, // TODO: UPDATE            // Max # of seconds to wait for response data
     refreshRate: 3, // # of seconds to wait between response polling
     maxBlobSizeBytes: 20000, // TODO: UPDATE            // Max expected size of file data in message (must be lower than device message buffer as it does not account for other message data)
